0f7d0440a2c34d52d390c1a56f3e56355791e99e
bartosz121 <bmagiera121@gmail.com>
2025-04-21 18:22:21 +0200

    Add job to lint, typecheck and test, don't add prometheus middleware if testing

--- todo_api/core/middleware/configure.py
+++ todo_api/core/middleware/configure.py
@@ -1,10 +1,14 @@
 import uuid
 
+import structlog
 from fastapi import FastAPI
 
+from todo_api.core.config import settings
 from todo_api.core.middleware.logging import LoggingMiddleware
 from todo_api.core.middleware.prometheus import PrometheusMiddleware
 from todo_api.core.middleware.request_id import RequestIdMiddleware
+
+logger: structlog.stdlib.BoundLogger = structlog.get_logger()
 
 
 def configure(app: FastAPI) -> None:
@@ -13,5 +17,9 @@ def configure(app: FastAPI) -> None:
         header_name="x-request-id",
         id_factory=lambda _: str(uuid.uuid4()),
     )
-    app.add_middleware(PrometheusMiddleware)
+
+    if not settings.ENVIRONMENT.is_testing:
+        logger.info("Prometheus middleware enabled")
+        app.add_middleware(PrometheusMiddleware)
+
     app.add_middleware(LoggingMiddleware)
